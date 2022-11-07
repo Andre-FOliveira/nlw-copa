24a807c4cee867f7bd45abc8c673d06726b5fab0
André Faustino <115813646+Andre-FOliveira@users.noreply.github.com>
2022-11-07 10:42:32 -0300

    Update main.js

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ function createCard(date, day, fase, games) {
 
 document.querySelector("#cards").innerHTML =
 
-    createCard("20/11", "domingo", "Fase de Grupo", createGame("Catar", "13:00", "Equador")) +
+    createCard("20/11", "domingo", "Fase de Grupo", createGame("catar", "13:00", "equador")) +
 
     createCard("21/11", "segunda", "Fase de Grupo", createGame("Inglaterra", "10:00", "Ira") +
         createGame("Senegal", "13:00", "Holanda") +
