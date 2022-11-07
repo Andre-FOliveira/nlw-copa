7564953ad4a3fd0c8e7eba5270fc2aeaccda1573
André Faustino <115813646+Andre-FOliveira@users.noreply.github.com>
2022-11-07 12:01:32 -0300

    Update main.js
    Ajustes no nome das bandeiras js

--- main.js
+++ main.js
@@ -32,54 +32,54 @@ function createCard(date, day, fase, games) {
 
 document.querySelector("#cards").innerHTML =
 
-    createCard("20/11", "domingo", "Fase de Grupo", createGame("catar", "13:00", "equador")) +
+    createCard("20/11", "domingo", "Fase de Grupo", createGame("Catar", "13:00", "Equador")) +
 
-    createCard("21/11", "segunda", "Fase de Grupo", createGame("inglaterra", "10:00", "ira") +
+    createCard("21/11", "segunda", "Fase de Grupo", createGame("inglaterra", "10:00", "Ira") +
         createGame("Senegal", "13:00", "Holanda") +
         createGame("Usa", "16:00", "Gales")) +
 
-    createCard("22/11", "terça", "Fase de Grupo", createGame("argentina", "07:00", "arabia") +
+    createCard("22/11", "terça", "Fase de Grupo", createGame("Argentina", "07:00", "Arabia") +
         createGame("Dinamarca", "10:00", "Tunisia") +
         createGame("Mexico", "13:00", "Polonia") +
         createGame("Franca", "16:00", "Australia")) +
 
-    createCard("23/11", "quarta", "Fase de Grupo", createGame("marrocos", "07:00", "croacia") +
+    createCard("23/11", "quarta", "Fase de Grupo", createGame("Marrocos", "07:00", "Croacia") +
         createGame("Alemanha", "10:00", "Japao") +
         createGame("Espanha", "13:00", "Costarica") +
         createGame("Belga", "16:00", "Canada")) +
 
 
-    createCard("24/11", "quinta", "Fase de Grupo", createGame("suica", "07:00", "camaroes") +
+    createCard("24/11", "quinta", "Fase de Grupo", createGame("Suica", "07:00", "Camaroes") +
         createGame("Uruguai", "10:00", "Corea") +
         createGame("Portugal", "13:00", "Gana") +
         createGame("Brasil", "16:00", "Servia")) +
 
-    createCard("25/11", "sexta", "Fase de Grupo", createGame("gales", "07:00", "ira") +
+    createCard("25/11", "sexta", "Fase de Grupo", createGame("Gales", "07:00", "Ira") +
         createGame("Catar", "10:00", "Senegal") +
         createGame("Holanda", "13:00", "Equador") +
         createGame("Inglaterra", "16:00", "Usa")) +
 
-    createCard("26/11", "sabado", "Fase de Grupo", createGame("tunisia", "07:00", "australia") +
+    createCard("26/11", "sabado", "Fase de Grupo", createGame("Tunisia", "07:00", "Australia") +
         createGame("Polonia", "10:00", "Arabia") +
         createGame("Franca", "13:00", "Dinamarca") +
         createGame("Argentina", "16:00", "Mexico")) +
 
-    createCard("27/11", "domingo", "Fase de Grupo", createGame("japao", "07:00", "costarica") +
+    createCard("27/11", "domingo", "Fase de Grupo", createGame("Japao", "07:00", "Costarica") +
         createGame("Belga", "10:00", "Marrocos") +
         createGame("Croacia", "13:00", "Canada") +
         createGame("Espanha", "16:00", "Alemanha")) +
 
-    createCard("28/11", "segunda", "Fase de Grupo", createGame("camaroes", "07:00", "servia") +
+    createCard("28/11", "segunda", "Fase de Grupo", createGame("Camaroes", "07:00", "Servia") +
         createGame("Corea", "10:00", "Gana") +
         createGame("Brasil", "13:00", "Suica") +
         createGame("Portugal", "16:00", "Uruguai")) +
 
-    createCard("29/11", "teça", "Fase de Grupo", createGame("equador", "12:00", "senegal") +
+    createCard("29/11", "teça", "Fase de Grupo", createGame("Equador", "12:00", "Senegal") +
         createGame("Holanda", "12:00", "Catar") +
         createGame("Ira", "16:00", "Usa") +
         createGame("Gales", "16:00", "Inglaterra")) +
 
-    createCard("30/11", "quarta", "Fase de Grupo", createGame("tunisia", "12:00", "franca") +
+    createCard("30/11", "quarta", "Fase de Grupo", createGame("Tunisia", "12:00", "Franca") +
         createGame("Australia", "12:00", "Dinamarca") +
         createGame("Polonia", "16:00", "Argentina") +
         createGame("Arabia", "16:00", "Mexico")) +
