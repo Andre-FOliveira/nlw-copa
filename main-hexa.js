33c88e4cec851a605bd3b69fd52065cf98450aa9
André Faustino <115813646+Andre-FOliveira@users.noreply.github.com>
2022-11-22 12:01:39 -0300

    Update main-hexa.js
    Adicionado item placar

--- main-hexa.js
+++ main-hexa.js
@@ -17,55 +17,67 @@ function createGame(player1, hour, player2) {
 }
 
 let delay = -0.4;
-function createCard(date, day, fase, games) {
+
+function createCard(date, day, fase, games, placar) {
     delay = delay + 0.4;
     return `
         <div class="card" style="animation-delay: ${delay}s">
             <h2>${date} <span>${day}</span></h2>
             <h3> ${fase}</h3>
             <ul>
-                ${games}     
+                ${games}
+                ${placar}        
             </ul>
         </div>
     `
 }
 
+function createResultado(res) {
+    return `
+                
+        <strong class="pl">${res}</strong>
+    
+    `
+}
+
+
 document.querySelector("#cards").innerHTML =
 
-    createCard("24/11", "quinta", "Fase de Grupo", 
-        
-        createGame("Brasil", "16:00", "Servia")) +
+    createCard("24/11", "quinta", "Fase de Grupo",
 
-    createCard("28/11", "segunda", "Fase de Grupo", 
-        createGame("Brasil", "13:00", "Suica")) +
-        
-    createCard("02/12", "sexta", "Fase de Grupo", 
-        createGame("Camaroes", "16:00", "Brasil")) 
+        createGame("Brasil", "16:00", "Servia"), createResultado("-   x   -")) +
 
-   /* createCard("03/12", "sabado", "Oitavas de Final", createGame("confirmar", "12:00", "confirmar") +
-        createGame("confirmar", "16:00", "confirmar")) +
+    createCard("28/11", "segunda", "Fase de Grupo",
+        createGame("Brasil", "13:00", "Suica"), createResultado("-   x   -")) +
 
-    createCard("04/12", "domingo", "Oitavas de Final", createGame("confirmar", "12:00", "confirmar") +
-        createGame("confirmar", "16:00", "confirmar")) +
+    createCard("02/12", "sexta", "Fase de Grupo",
+        createGame("Camaroes", "16:00", "Brasil"), createResultado("-   x   -"))
 
-    createCard("05/12", "segunda", "Oitavas de Final", createGame("confirmar", "12:00", "confirmar") +
-        createGame("confirmar", "16:00", "confirmar")) +
+/*  createCard("03/12", "sabado", "Oitavas de Final", createGame("confirmar", "12:00", "confirmar"), createResultado("-  x  - ")+
+    createGame("confirmar", "16:00", "confirmar")+createResultado("-  x  - ")) +
 
-    createCard("06/12", "terça", "Oitavas de Final", createGame("confirmar", "12:00", "confirmar") +
-        createGame("confirmar", "16:00", "confirmar")) +
+    createCard("04/12", "domingo", "Oitavas de Final", createGame("confirmar", "12:00", "confirmar"),createResultado("-  x  - ")+
+    createGame("confirmar", "16:00", "confirmar")+createResultado("-  x  - ")) +
 
-    createCard("09/12", "sexta", "Quartas de Final", createGame("confirmar", "12:00", "confirmar") +
-        createGame("confirmar", "16:00", "confirmar")) +
+    createCard("05/12", "segunda", "Oitavas de Final", createGame("confirmar", "12:00", "confirmar"),createResultado("-  x  - ")+
+        createGame("confirmar", "16:00", "confirmar")+createResultado("-  x  - ")) +
 
-    createCard("10/12", "sabado", "Quartas de Final", createGame("confirmar", "12:00", "confirmar") +
-        createGame("confirmar", "16:00", "confirmar")) +
+    createCard("06/12", "terça", "Oitavas de Final", createGame("confirmar", "12:00", "confirmar"),createResultado("-  x  - ")+
+        createGame("confirmar", "16:00", "confirmar")+createResultado("-  x  - ")) +
 
-    createCard("13/12", "terça", "Semifinal", createGame("confirmar", "16:00", "confirmar")) +
+    createCard("09/12", "sexta", "Quartas de Final", createGame("confirmar", "12:00", "confirmar"),createResultado("-  x  - ")+
+        createGame("confirmar", "16:00", "confirmar")+createResultado("-  x  - ")) +
 
-    createCard("14/12", "quarta", "Semifinal", createGame("confirmar", "16:00", "confirmar")) +
+    createCard("10/12", "sabado", "Quartas de Final", createGame("confirmar", "12:00", "confirmar"),createResultado("-  x  - ")+
+        createGame("confirmar", "16:00", "confirmar")+createResultado("-  x  - ")) +
 
-    createCard("17/12", "sabado", "Disputa 3º Lugar", createGame("confirmar", "12:00", "confirmar")) +
+    createCard("13/12", "terça", "Semifinal", createGame("confirmar", "16:00", "confirmar"),createResultado("-  x  - ")) +
+
+    createCard("14/12", "quarta", "Semifinal", createGame("confirmar", "16:00", "confirmar"),createResultado("-  x  - ")) +
+
+    createCard("17/12", "sabado", "Disputa 3º Lugar", createGame("confirmar", "12:00", "confirmar"),createResultado("-  x  - ")) +
 
 
-    createCard("18/12", "domingo", "final", createGame("confirmar", "12:00", "confirmar"))*/
+    createCard("18/12", "domingo", "final", createGame("confirmar", "12:00", "confirmar"),createResultado("-  x  - "))
 
+    */
