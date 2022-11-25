516e11cccb0fc536693b539e610776c1007b50b7
André Faustino <115813646+Andre-FOliveira@users.noreply.github.com>
2022-11-25 12:32:34 -0300

    Update main.js
    Atualização de placar Qatar x Senegal

--- main.js
+++ main.js
@@ -66,7 +66,7 @@ document.querySelector("#cards").innerHTML =
         createGame("Brasil", "16:00", "Servia") + createResultado("2  x  0"))+
 
     createCard("25/11", "sexta", "Fase de Grupo", createGame("Gales", "07:00", "Ira"), createResultado("0  x  2")+
-        createGame("Catar", "10:00", "Senegal") + createResultado("-  x  - ")+
+        createGame("Catar", "10:00", "Senegal") + createResultado("1  x  3")+
         createGame("Holanda", "13:00", "Equador") + createResultado("-  x  - ")+
         createGame("Inglaterra", "16:00", "Usa")+ createResultado("-  x  - ")) +
 
