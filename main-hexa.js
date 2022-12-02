47d336770e4a5f9d5d7485bff572b22bf054d6a7
André Faustino <115813646+Andre-FOliveira@users.noreply.github.com>
2022-12-02 18:12:41 -0300

    Update main-hexa.js

--- main-hexa.js
+++ main-hexa.js
@@ -53,8 +53,7 @@ document.querySelector("#cards").innerHTML =
     createCard("02/12", "sexta", "Fase de Grupo",
         createGame("Camaroes", "16:00", "Brasil"), createResultado("1   x   0")) +
     
-    createCard("05/12", "segunda", "Oitavas de Final", createGame("confirmar", "12:00", "confirmar"),createResultado("-  x  - ")+
-        createGame("Brasil", "16:00", "Corea")+createResultado("-  x  - ")) 
+    createCard("05/12", "segunda", "Oitavas de Final", createGame("Brasil", "16:00", "Corea")+createResultado("-  x  - ")) 
 
 /*  createCard("03/12", "sabado", "Oitavas de Final", createGame("confirmar", "12:00", "confirmar"), createResultado("-  x  - ")+
     createGame("confirmar", "16:00", "confirmar")+createResultado("-  x  - ")) +
